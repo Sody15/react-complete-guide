050efd21541cb17ef379fddf136ecdb8256842b2
Sody15 <pdsoderb@gmail.com>
2023-02-06 16:00:24 -0800

    changes

--- age-app/src/components/Popup/Popup.js
+++ age-app/src/components/Popup/Popup.js
@@ -1,21 +1,42 @@
 import Button from "../UI/Button";
 import classes from "./Popup.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.closePopup}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.popup}>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button onClick={props.closePopup}>Okay</Button>
+      </footer>
+    </div>
+  );
+};
 
 const Popup = (props) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={props.closePopup}></div>
-      <div className={classes.popup}>
-        <header>
-          <h2>{props.title}</h2>
-        </header>
-        <div>
-          <p>{props.message}</p>
-        </div>
-        <footer>
-          <Button onClick={props.closePopup}>Okay</Button>
-        </footer>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop closePopup={props.closePopup} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          closePopup={props.closePopup}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
