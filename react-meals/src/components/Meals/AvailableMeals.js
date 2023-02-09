2e37673a04832cd7a323e493f98e33444095f8b4
Sody15 <pdsoderb@gmail.com>
2023-02-09 11:21:11 -0800

    changes

--- react-meals/src/components/Meals/AvailableMeals.js
+++ react-meals/src/components/Meals/AvailableMeals.js
@@ -1,15 +1,27 @@
 import MEALS_DATA from "../../dummy-meals";
+import Card from "../UI/Card";
 
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const mealsList = MEALS_DATA.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meal}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
