a1d0e782848f9074a1e1c6e519b0673d718fcd4d
Sody15 <pdsoderb@gmail.com>
2023-02-11 17:49:07 -0800

    changes

--- class-based-components/src/App.js
+++ class-based-components/src/App.js
@@ -1,9 +1,22 @@
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/UserContext';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
     <div>
-      <Users />
+      <UsersContext.Provider value={usersContext}>
+        <UserFinder />
+      </UsersContext.Provider>
     </div>
   );
 }
