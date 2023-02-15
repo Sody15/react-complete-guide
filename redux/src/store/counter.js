c73e958dbdd6e410cf97b3b045745170aec2b0ad
Sody15 <pdsoderb@gmail.com>
2023-02-15 13:57:31 -0800

    changes

---
+++ redux/src/store/counter.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, amount: 1, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrement(state, action) {
+      state.counter = state.counter - action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === 'decrement') {
+//     return {
+//       counter: state.counter - action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === 'toggle') {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// store
+// const counterStore = createStore(counterReducer);
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
