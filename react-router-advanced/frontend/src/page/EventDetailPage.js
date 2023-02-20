d8a33062a518f8ef3c4b1a67ba6ed5649837f33c
Sody15 <pdsoderb@gmail.com>
2023-02-20 11:17:34 -0800

    changes

--- react-router-advanced/frontend/src/page/EventDetailPage.js
+++ react-router-advanced/frontend/src/page/EventDetailPage.js
@@ -1,14 +1,78 @@
-import { useParams } from 'react-router-dom';
+import { Suspense } from 'react';
+import {
+  Await,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+} from 'react-router-dom';
+
+import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 const EventDetailPage = () => {
-  const params = useParams();
+  const { event, events } = useRouteLoaderData('event-detail');
 
   return (
     <>
-      <h1>Event Detail</h1>
-      <p>{params.id}</p>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 };
 
 export default EventDetailPage;
+
+const loadEvent = async (id) => {
+  const response = await fetch(`http://localhost:8080/events/${id}`);
+
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not fetch deteails for selected event.' },
+      { status: 500 }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.event;
+  }
+};
+
+const loadEvents = async () => {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    return json({ message: 'Could not fetch events' }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
+
+export const loader = async ({ request, params }) => {
+  const id = params.id;
+
+  return defer({
+    event: await loadEvent(id),
+    events: await loadEvents(),
+  });
+};
+
+export const action = async ({ params, request }) => {
+  const id = params.id;
+  const response = await fetch('http://localhost:8080/events/' + id, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: 'Could not delete event.' }, { status: 500 });
+  }
+  return redirect('/events');
+};
--- react-router-advanced/frontend/src/router.js
+++ react-router-advanced/frontend/src/router.js
@@ -3,27 +3,62 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from './components/Root';
 
 import EditEventPage from './page/EditEventPage';
-import EventDetailPage from './page/EventDetailPage';
-import EventsPage from './page/EventsPage';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventaction,
+} from './page/EventDetailPage';
+import EventsPage, { loader as eventsLoader } from './page/Event';
 import EventsRootLayout from './page/EventsRoot';
 import HomePage from './page/HomePage';
 import NewEventPage from './page/NewEventPage';
+import ErrorPage from './page/Error';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './page/Newletter';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: 'events',
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
-          { path: 'new', element: <NewEventPage /> },
-          { path: ':id', element: <EventDetailPage /> },
-          { path: ':id/edit', element: <EditEventPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+          {
+            path: ':id',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventaction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
