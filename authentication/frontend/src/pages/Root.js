d8edd4385663ee83d13229912f93954fcb0efb9d
Sody15 <pdsoderb@gmail.com>
2023-02-20 15:44:30 -0800

    changes

--- authentication/frontend/src/pages/Root.js
+++ authentication/frontend/src/pages/Root.js
@@ -1,9 +1,29 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
